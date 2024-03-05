42184c3a5c129f8d837d28c592fdfc2f86f68044
kumarkpavan6 <kumarkpavan6@gmail.com>
2024-03-04 20:38:10 -0500

    Create endpoint, display retrieved data
    Created /get-red-light-camera

--- index.js
+++ index.js
@@ -1,9 +1,51 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const https = require('https');
+const { spawn } = require('child_process');
+
+// Set up a route to serve the information
 app.get('/', (req, res) => {
-  res. send('Hello World!')
-})
-app. listen(port, () => {
-  console.log(`Listening port on ${port}`)
-})
+    res.send('Information will be displayed here2');
+});
+
+// Set up a route to serve the information
+app.get('/get-red-light-cameras', (req, res) => {
+  // Call the separate Node.js script
+  const process = spawn('node', ['retrieve-red-light-cameras.js']);
+
+  process.stdout.on('data', (data) => {
+      // Send the data to the client
+      res.send(data.toString());
+  });
+
+  process.stderr.on('data', (data) => {
+      console.error(`stderr: ${data}`);
+  });
+
+  process.on('close', (code) => {
+      console.log(`child process exited with code ${code}`);
+  });
+});
+
+// Start the server
+const port = 3000;
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
+
+// Call the separate Node.js script every 10 seconds
+setInterval(() => {
+    const process = spawn('node', ['retrieve-red-light-cameras.js']);
+
+    process.stdout.on('data', (data) => {
+        console.log(`stdout: ${data}`);
+    });
+
+    process.stderr.on('data', (data) => {
+        console.error(`stderr: ${data}`);
+    });
+
+    process.on('close', (code) => {
+        console.log(`child process exited with code ${code}`);
+    });
+}, 10000);
